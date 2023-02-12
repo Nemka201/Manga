9bae1d9a9ee203f07ed9162164132339c1273614
Isaias RoblesVega <isak4567@gmail.com>
2023-02-11 23:23:25 -0300

    Comienzo de Class Tendencia

--- Manga/Models/Tendencia.cs
+++ Manga/Models/Tendencia.cs
@@ -3,6 +3,19 @@
     public partial class Tendencia
     {
         public List<Serie> seriales { get; set; }
-        public List<Capitulo> capitulos { get; set; }
+        public List<Capitulo> capitulos { get; set; } = new List<Capitulos>();
+
+        public Tendencia(List<Series> sL, List<Capitulos> cL )
+        {
+            seriesList = sL.OrderByDescending(x => x.Favoritos)
+                .ThenByDescending(x => x.Idserie).ToList().GetRange(0, 3);
+
+            foreach (var item in seriesList)
+            {
+                capList.Add(cL
+                    .Find( x => x.Idserie == item.Idserie 
+                    && item.Capitulos.ToString() == x.Titulo));
+            }
+        }
     }
 }
